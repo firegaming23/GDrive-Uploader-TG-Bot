9f45adf2b241fd443fb89918dd75874c4ce1fc37
firegaming23 <princeyadav10668@gmail.com>
2026-08-02 21:23:48 +0530

    Update token.py

--- plugins/token.py
+++ plugins/token.py
@@ -10,7 +10,7 @@ from helpers import parent_id_sql as sql
 OAUTH_SCOPE = "https://www.googleapis.com/auth/drive"
 REDIRECT_URI = "http://localhost"
 G_DRIVE_DIR_MIME_TYPE = "application/vnd.google-apps.folder"
-G_DRIVE_CLIENT_ID = "197036948433-4sjgjrj1osm5b5neu8khh7c2nsvn96f7.apps.googleusercontent.com"
+G_DRIVE_CLIENT_ID = "751038558683-qjamst56ahmeh68bbcr64tstpbkitvgd.apps.googleusercontent.com"
 G_DRIVE_CLIENT_SECRET = "dnXoMIu2V7HQ8G8RicrKmvlu"
 
 user_flows = {}
